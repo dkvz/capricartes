4301b06274fe1df6295692333e9845f60227746e
DkVZ <bouc@clanz.be>
2018-09-02 18:10:51 +0200

    The slides thingy actually works

--- src/slides.js
+++ src/slides.js
@@ -3,8 +3,13 @@ class Slides {
   constructor(slides, template, document) {
     this.slides = slides;
     this.document = document;
-    this.translateSize = 100;
-    this.dom = template.content.cloneNode(true);
+    this.transformDuration = 1.2;
+    // template.content is a documentFragment, and inserting a 
+    // document fragment completely empties it.
+    // I wanted to keep references to the nodes in the current object,
+    // so I have to get the direct child element inside the template
+    // and use that as the 'dom'.
+    this.dom = template.content.cloneNode(true).querySelector('.slides');
     // Bind everything:
     this.dom.querySelector('[data-action="close"]')
       .addEventListener('click', this.hide.bind(this));
@@ -19,12 +24,12 @@ class Slides {
     this.slideNodes = this.slides.map((s, i) => {
       const nel = this.document.createElement('div');
       nel.textContent = s;
+      nel.style.transition = 'transform ' + this.transformDuration + 's';
       slidesEl.appendChild(nel);
-      nel.style.transition = 'transform 2s';
       if (i === 0) {
         // Only the first element has to be on top
         // and shown.
-        nel.style.transform = 'translateX(' + this.translateSize + 'px)';
+        nel.style.transform = 'translateX(100%)';
         this._applyShownState(nel);
       } else {
         this._applyHiddenState(nel);
@@ -40,26 +45,58 @@ class Slides {
   }
 
   _applyShownState(el) {
+    el.style.display = '';
+    //el.style.transition = 'transform ' + this.transformDuration + 's';
     el.style.zIndex = 2;
-    el.style.transform = 'translateX(0px)';
+    setTimeout(_ => {
+      el.style.transform = 'translateX(0px)';
+    }, 100);
   }
 
   _applyHiddenState(el) {
-    el.style.zIndex = -1;
+    el.style.display = 'none';
+    el.style.zIndex = 0;
+  }
+
+  _preTranslate(fromLeft) {
+    //this.slideNodes[this.currentSlide].style.transition = '';
+    this.slideNodes[this.currentSlide].style.transform =
+      'translateX(' + (fromLeft ? '-' : '') +
+      '100%)';
+  }
+
+  _slide() {
+    this.slideNodes.forEach((s, i) => {
+      if (i === this.currentSlide) {
+        this._applyShownState(s);
+      } else {
+        this._applyHiddenState(s);
+      }
+    });
   }
 
   next() {
     // We have to check the current position
     // using this.currentSlide.
-    
+    if (++this.currentSlide >= this.slides.length) {
+      this.currentSlide = 0;
+    }
+    this._preTranslate();
+    this._slide();
   }
 
   previous() {
-
+    if (--this.currentSlide < 0) {
+      this.currentSlide = (this.slides.length - 1);
+    }
+    this._preTranslate(true);
+    this._slide();
   }
 
   hide() {
-
+    // We set a transition on opacity on the parent
+    // 'this.dom' here.
+    this.dom.style.opacity = 0;
   }
 
   attach(element) {
