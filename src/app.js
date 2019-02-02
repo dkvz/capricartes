623a489aa165488a29240bbee7cf76a346a633eb
DkVZ <bouc@clanz.be>
2019-02-02 13:12:33 +0100

    Changed color theme, old browser detection should work now

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import './styles/base.scss';
 // This should exclude IE 11.
 import './browser-detect';
 if (!window.Modernizr.arrow || !window.Modernizr.promises) {
-  window.alert('Warning: this app won\'t work on your browser. Please download a modern browser.');
+  document.body.innerHTML = 'Warning: this app won\'t work on your browser. Please download a modern browser.';
 }
 
 import Capricartes from './capricartes';
